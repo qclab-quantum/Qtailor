b2143bd93942cbda176b120824f851b1e1c4ada0
天歌 <felix.litian@foxmail.com>
2023-10-30 22:02:11 +0800

    --

--- main.py
+++ main.py
@@ -16,54 +16,10 @@ from tianshou.utils import TensorboardLogger, WandbLogger
 from tianshou.utils.net.common import Net
 from tianshou.utils.net.continuous import Actor, Critic
 from torch.utils.tensorboard import SummaryWriter
-
+from config_private import get_args
 
 # !/usr/bin/envs python3
 
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--task", type=str, default='MysqlDB-v0')
-    parser.add_argument("--seed", type=int, default=12345)
-    parser.add_argument("--buffer-size", type=int, default=10000)
-    parser.add_argument("--hidden-sizes", type=int, nargs="*", default=[256,256, 256])
-    parser.add_argument("--actor-lr", type=float, default=1e-3)
-    parser.add_argument("--critic-lr", type=float, default=1e-3)
-    parser.add_argument("--gamma", type=float, default=0.99)
-    parser.add_argument("--tau", type=float, default=0.005)
-    parser.add_argument("--exploration-noise", type=float, default=0.1)
-    parser.add_argument("--start-timesteps", type=int, default=25000)
-    parser.add_argument("--epoch", type=int, default=20000)
-    parser.add_argument("--step-per-epoch", type=int, default=2000)
-    parser.add_argument("--step-per-collect", type=int, default=1)
-    parser.add_argument("--update-per-step", type=int, default=1)
-    parser.add_argument("--n-step", type=int, default=1)
-    parser.add_argument("--batch-size", type=int, default=256)
-    parser.add_argument("--training-num", type=int, default=1)
-    parser.add_argument("--test-num", type=int, default=1)
-    parser.add_argument("--logdir", type=str, default="log")
-    parser.add_argument("--render", type=float, default=0.)
-    parser.add_argument(
-        "--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu"
-    )
-    parser.add_argument("--resume-path", type=str, default=None)
-    parser.add_argument("--resume-id", type=str, default=None)
-    parser.add_argument(
-        "--logger",
-        type=str,
-        default="tensorboard",
-        choices=["tensorboard", "wandb"],
-    )
-    #parser.add_argument("--wandb-project", type=str, default="mujoco.benchmark")
-    parser.add_argument(
-        "--watch",
-        default=True,
-        action="store_true",
-        help="watch the play of pre-trained policy only",
-    )
-
-
-    return parser.parse_args()
 
 def make_env(task,seed,training_num,test_num):
     env = gym.make(task)
