013c89e2193e9ba8437a327fc004188f22a8205e
天歌 <felix.litian@foxmail.com>
2023-12-20 11:11:57 +0800

    添加 csv 工具类

---
+++ utils/csv_util.py
@@ -0,0 +1,45 @@
+import csv
+import os
+
+class CSVUtil:
+    @staticmethod
+    def write_data(file_path, data):
+        # 确保文件夹存在
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        # 写入数据
+        with open(file_path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
+    @staticmethod
+    def append_data(file_path, data):
+        # 确保文件夹存在
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        # 追加数据
+        with open(file_path, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
+    @staticmethod
+    def read_data(file_path):
+        data = []
+        # 读取数据
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                data.append(row)
+        return data
+
+if __name__ == '__main__':
+    # 写入数据
+    data = [['Name', 'Age'], ['Alice', 25], ['Bob', 30]]
+    CSVUtil.write_data('d:/data.csv', data)
+
+    # 追加数据
+    new_data = [['Charlie', 35]]
+    CSVUtil.append_data('d:/data.csv', new_data)
+
+    # 读取数据
+    data = CSVUtil.read_data('d:/data.csv')
+    for row in data:
+        print(row)
