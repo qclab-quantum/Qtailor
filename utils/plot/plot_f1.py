2aebfc669adabcd910337efdbe011d8ddc8ebedc
天歌 <felix.litian@foxmail.com>
2024-03-13 15:46:30 +0800

    add plot_f1

--- utils/plot/plot_f1.py
+++ utils/plot/plot_f1.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import re
 from utils.file.excel_util import ExcelUtil
-
+mpl.rcParams['font.family'] = ['Arial']
+mpl.rcParams['font.size'] = 12
 # 假设我们有9组数据，每组都有两个子组
 # data = {
 #     'Group1': ([12, 30, 1, 8, 22], [28, 6, 16, 5, 10]),
@@ -45,6 +47,8 @@ index = np.arange(len(labels))
 
 # 创建3x3的子图布局
 fig, axes = plt.subplots(3, 3, figsize=(15, 10))  # figsize可以根据需要调整
+
+plt.subplots_adjust(hspace=0.4,wspace = 0.2)
 # 遍历数据和子图网格，绘制柱状图
 for i, (group_name, (group1, group2,group3)) in enumerate(data.items()):
     # 计算子图的行和列索引
@@ -55,26 +59,28 @@ for i, (group_name, (group1, group2,group3)) in enumerate(data.items()):
     ax = axes[row, col]
 
     # 绘制第一组数据的柱状图
-    ax.bar(index, group1,  bar_width,color = '#5370c4',label=f'{group_name} 1',hatch='o', edgecolor='black')
+    ax.bar(index, group1,  bar_width,color = '#5370c4',label=f'{group_name} 1',hatch='-', edgecolor='black')
 
     # 绘制第二组数据的柱状图
-    ax.bar(index + bar_width, group2, bar_width,color = '#f16569', label=f'{group_name} 2',hatch='\\\\', edgecolor='black')
-    ax.bar(index + bar_width*2, group3, bar_width,color = '#95c978', label=f'{group_name} 3',hatch='\\\\', edgecolor='black')
+    ax.bar(index + bar_width, group2, bar_width,color = '#f16569', label=f'{group_name} 2',hatch='.', edgecolor='black')
+    ax.bar(index + bar_width*2, group3, bar_width,color = '#95c978', label=f'{group_name} 3',hatch='//', edgecolor='black')
 
     # 添加图例
-    ax.legend(['Reinforce','Qiskit','Mix'], loc='best')
+    ax.legend(['Reinforce','Qiskit','Mix'], loc='upper left')
 
     # 设置横轴的标签
     ax.set_xticks(index + bar_width / 1)
     ax.set_xticklabels(labels_2d[i])
     if i ==0 or i==3:
-        ax.set_ylabel('111')
+        ax.set_ylabel('Depth')
+    if i in range(6,8):
+        ax.set_xlabel('bits')
     # 设置图表的标题
     ax.set_title(f'{group_name}')
     # 显示背景网格
     ax.grid(True, which='both', axis='y', linestyle='--', linewidth=0.5)
 # 调整子图之间的间距
-plt.tight_layout()
+#plt.tight_layout()
 
 # 显示图表
 plt.show()
